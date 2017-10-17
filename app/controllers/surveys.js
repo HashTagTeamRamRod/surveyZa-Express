104a5412e458987dd5b41619a6380a35d0999236
cmguido <chris.m.guido@gmail.com>
2017-10-17 15:52:28 -0400

    Add working patch for non _owner votes
    In surveys controller, if conditional checks for user, so users can
    still update their entire survey, while non owners can only vote.
    
    Contributors:
    Christopher Guido, Jenny Yee, Melissa Luna

--- app/controllers/surveys.js
+++ app/controllers/surveys.js
@@ -37,22 +37,31 @@ const create = (req, res, next) => {
 }
 
 const update = (req, res, next) => {
-  delete req.body._owner  // disallow owner reassignment
+  const currentUserId = req.body._owner
+
+  if (currentUserId === req.body._owner) {
+    delete req.body._owner  // disallow owner reassignment
   // const id = { _id: req.params.id }
   // console.log('req survey is ', req.survey)
   // console.log('req questions is ', req.survey.questions[0]._id)
   // console.log('req response is ', req.survey.questions[0].responses[0].answer1)
   // console.log('req responses are ', req.survey.questions.responses)
-  console.log('body is ', req.body)
+    const question = req.body.surveys.questions
 
-  const question = req.body.surveys.questions
+    const response = req.body.surveys.responses
 
-  const response = req.body.surveys.responses
-
-  req.survey.update({ $set: { questions: question, responses: response } })
+    req.survey.update({ $set: { questions: question, responses: response } })
     .then(console.log)
     .then(() => res.sendStatus(204))
     .catch(next)
+  } else {
+    const response = req.body.surveys.responses
+
+    req.survey.update({ $set: { responses: response } })
+      .then(console.log)
+      .then(() => res.sendStatus(204))
+      .catch(next)
+  }
 }
 // const update = (req, res, next) => {
 //   console.log('params is ', req.params.id)
@@ -80,6 +89,6 @@ module.exports = controller({
 }, { before: [
   { method: setUser, only: ['index', 'show'] },
   { method: authenticate, except: ['index', 'show'] },
-  { method: setModel(Survey), only: ['show'] },
-  { method: setModel(Survey, { forUser: true }), only: ['update', 'destroy'] }
+  { method: setModel(Survey), only: ['update', 'show'] },
+  { method: setModel(Survey, { forUser: true }), only: ['destroy'] }
 ] })
